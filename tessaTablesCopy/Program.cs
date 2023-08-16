b40c82fee9894e3bba5645e0b815b46d8c98930c
Гараев Рустам <r.garaev@reo.ru>
2023-08-16 16:53:08 +0400

    фикс подмены ID

--- tessaTablesCopy/Program.cs
+++ tessaTablesCopy/Program.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Linq;
 
 namespace tessaTablesCopy
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             string path;
             string fileName;
+            string newFileName;
             string text;
             string pathFileCopied;
             string fileExtension = ".tst";
@@ -18,8 +20,17 @@ namespace tessaTablesCopy
 
             Console.WriteLine("Specify the path:");
             path = Console.ReadLine();
+
             Console.WriteLine("Specify the file name:");
             fileName = Console.ReadLine();
+
+            Console.WriteLine("Specify the new file name:");
+            newFileName = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(newFileName))
+            {
+                newFileName = fileName + "Copy";
+            }
 
             try
             {
@@ -28,17 +39,39 @@ namespace tessaTablesCopy
 
                 if (!File.Exists(pathCombine))
                 {
-                    fileName += fileExtension;
-                    pathCombine = Path.Combine(path, fileName);
+                    pathCombine = Path.Combine(path, fileName + fileExtension);
                 }
 
                 using StreamReader reader = new StreamReader(pathCombine);
                 text = reader.ReadToEnd();
                 string output;
+                string[] words = text.Split(' ');
+                text = text.Replace(fileName, newFileName);
+                words = text.Split(' ');
 
-                foreach (var word in text.Split(' '))
+                List<string> duplicatesIds = new List<string>();
+
+                foreach (var word in words)
                 {
                     if (word.Contains("ID=") && word.Length <= 42)
+                    {
+                        var duplicatesId = word.Replace("ID=", string.Empty).Replace("\"", string.Empty);
+
+                        if (!duplicatesIds.Any(x => x == duplicatesId) && words.Count(x => x.Contains(duplicatesId)) > 1)
+                        {
+                            string tempTableID = Guid.NewGuid().ToString();
+                            text = text.Replace(duplicatesId, tempTableID);
+                            duplicatesIds.Add(tempTableID);
+                            duplicatesIds.Add(duplicatesId);
+                        }
+                    }
+                }
+
+                words = text.Split(' ');
+
+                foreach (var word in words)
+                {
+                    if (!duplicatesIds.Any(x => word.Contains(x)) && word.Contains("ID=") && word.Length <= 42)
                     {
                         output = $"ID=\"{Guid.NewGuid()}\"";
                     }
@@ -50,17 +83,18 @@ namespace tessaTablesCopy
                     textCopiedArray.Add(output);
                 }
 
-                int doteIndex = fileName.LastIndexOf(".");
+                newFileName += fileExtension;
+                int doteIndex = newFileName.LastIndexOf(".");
 
                 if (doteIndex > 0)
                 {
-                    fileExtension = fileName.Substring(fileName.LastIndexOf("."));
-                    fileName = fileName.Substring(0, fileName.LastIndexOf("."));
-                    pathFileCopied = Path.Combine(path, $"{fileName}Copy{fileExtension}");
+                    fileExtension = newFileName.Substring(newFileName.LastIndexOf("."));
+                    newFileName = newFileName.Substring(0, newFileName.LastIndexOf("."));
+                    pathFileCopied = Path.Combine(path, $"{newFileName}{fileExtension}");
                 }
                 else
                 {
-                    pathFileCopied = Path.Combine(path, $"{fileName}Copy");
+                    pathFileCopied = Path.Combine(path, $"{newFileName}");
                 }
 
 
